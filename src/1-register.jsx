611aa38595445973c490f39604a0b67b15f71630
Jakub Myrta <mkuba2006@gmail.com>
2023-10-29 21:56:44 +0100

    part1

--- src/1-register.jsx
+++ src/1-register.jsx
@@ -1,52 +1,64 @@
-import React, { useState, useEffect, useReducer } from 'react';
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState, useEffect, useRef } from 'react';
 import classes from './style.module.css';
-import myImage from './images/register.png';
+import myImage from './images/log4.png';
 import Passed from './passed';
-function Register(props){
 
-  const [nick, setnick]= useState('');
-  const [pass, setpass]= useState('');
-  const [pass2, setpass2]= useState('');
+function Register(props) {
+  const nickRef = useRef(null);
+  const passRef = useRef(null);
+  const pass2Ref = useRef(null);
+  const warnRef = useRef(false);
   const [show, setshow]= useState(false);
-  const [warn, setwarn]= useState(false);
 
-  const nickchange = (e)=>{
-    setnick(e.target.value);
-  }
-  const passchange = (e)=>{
-    setpass(e.target.value);
-  }
-  const passchange2 = (e)=>{
-    setpass2(e.target.value);
-  }
   useEffect(() => {
-    setwarn(nick !== '' && pass === pass2 && pass !== '');
-    console.log(warn);
-  }, [pass2]);
+    if (nickRef.current && passRef.current && pass2Ref.current) {
+      warnRef.current =
+        nickRef.current.value !== '' &&
+        passRef.current.value === pass2Ref.current.value &&
+        passRef.current.value !== '';
+      console.log(warnRef.current);
+      console.log(nickRef,passRef,passRef);
+    }
+  }, [pass2Ref]);
   
   const submit = (e) => {
     e.preventDefault();
-    setshow(true)
-    setTimeout(() => {
+      const nick = nickRef.current.value;
+      const pass = passRef.current.value;
+      const pass2 = pass2Ref.current.value;
+      setshow(true);
       props.onSub(nick, pass, pass2);
-    }, 2000);
-  }
-  
-  const cant =(e)=>{
-    e.preventDefault();
   }
 
-  return(
+
+  return (
     <div className={classes.cont}>
-      <img src={myImage} alt="My PNG Image" />
-      <h2>Create your account</h2>
-      <div className={classes.reg}>
-        <input type='text' placeholder='Nickname' id='nick' className={classes.input} onChange={nickchange}/>
-        <input type='password' placeholder='password' id='password' className={classes.input} onChange={passchange}/>
-        <input type='password' placeholder='confirm password' id='confirm_password' className={classes.input} onBlur={passchange2}  />
-        <input className={`${warn === true ? classes.warn2 : classes.input2}`} type="submit" id="submit" onClick={warn === true ? submit : cant}/>
+      <div className={classes.img}>
+        <img src={myImage} alt="My PNG Image" />
       </div>
-      {show===true && <Passed/>}
+      <main>
+        <h2>Welcome!</h2>
+        <h3>Create your account</h3>
+        <div className={classes.reg}>
+          <input type='text' placeholder='Nickname' id='nick' className={classes.input} ref={nickRef} />
+          <input type='password' placeholder='password' id='password' className={classes.input} ref={passRef} />
+          <input type='password' placeholder='confirm password' id='confirm_password' className={classes.input} ref={pass2Ref} />
+          <input className={`${warnRef.current === true ? classes.warn2 : classes.input2}`} type="submit" id="submit" onClick={submit} />
+          {show === true && <Passed />}
+        </div>
+      </main>
     </div>
   )
 }
